bffe2c9c21242d1b52e371249c127e8665a16e72
Lee Offir <ljotyd@mail.missouri.edu>
2018-01-31 17:20:27 -0600

    2

--- Swift Language Basics/main.swift
+++ Swift Language Basics/main.swift
@@ -13,32 +13,40 @@ import Foundation
 var sample1: UInt8 = 0x3A
 var sample2: UInt8 = 58
 var heartRate: Int = 85
-var deposits:Double = 135002796
-var acceleration:Float=9.800
-var mass :Float=14.6
-var distance :Double=129.763001
+var deposits: Double = 135002796
+var acceleration: Float=9.800
+var mass: Float=14.6
+var distance: Double=129.763001
 var lost: Bool=true
-var expensive :Bool=true
-var choice:Int=2
+var expensive: Bool=true
+var choice: Int=2
 let integral: Character = "\u{222B}"
 let greeting: String = "Hello"
 var name: String = "Karen"
 
-if sample1 == sample2 {
+if sample1 == sample2
+{
     print("The samples are equal")
-}else {
+}
+else
+{
     print("The samples are not equal")
 }
 
 if (heartRate >= 40) && (heartRate <= 80) {
     print("Heart rate is normal." )
-} else{
+}
+else
+{
     print("Heart rate is not normal.")
 }
 
-if (deposits >= 100000000) {
+if (deposits >= 100000000)
+{
     print("You are exceedingly wealthy.")
-}else{
+}
+else
+{
     print("Sorry you are so poor.")
 }
 
@@ -47,29 +55,35 @@ var force = mass*acceleration
 print("force = \(force)")
 print("\(distance) is the distance.")
 
-if lost && expensive {
+if lost && expensive
+{
     print("I am really sorry! I will get the manager.")
-}else if lost && !expensive {
+}
+else if lost && !expensive
+{
     print("Here is coupon for 10% off.")
 }
 
-switch choice {
-case 1 : print("You chose 1.")
-case 2 : print("You chose 2.")
-case 3 : print("You chose 3.")
+switch choice
+{
+    case 1 : print("You chose 1.")
+    case 2 : print("You chose 2.")
+    case 3 : print("You chose 3.")
     
-default: print("You made an unknown choice.")
+    default: print("You made an unknown choice.")
 }
 
 print("\(integral) is an integral.")
 
-for i in 5 ... 10{
+for i in 5 ... 10
+{
     print("i = \(i)")
 }
 
 var age: Int=0
-while age < 6 {
-    print("age = \(age)")
+while age < 6
+{
+    print("age =\(age)")
     age+=1
 }
 
